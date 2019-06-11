e1f588331328e967ae950f9e2ccc7c3f9cc43081
Gingano <lesha.kowalenko@gmail.com>
2019-06-11 19:28:35 +0300

    Updates

---
+++ precache-manifest.c4bfce5c517af040a0a37540987807f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c4b26360ab82f358ac559e87da5511",
+    "url": "/to_do_list/index.html"
+  },
+  {
+    "revision": "fb8f30d4df9fc1eeae5a",
+    "url": "/to_do_list/static/css/main.9093acdf.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/to_do_list/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "fb8f30d4df9fc1eeae5a",
+    "url": "/to_do_list/static/js/main.dc2d87b2.chunk.js"
+  },
+  {
+    "revision": "18bb48590e77e03941c4",
+    "url": "/to_do_list/static/js/runtime~main.015986e0.js"
+  }
+]);
