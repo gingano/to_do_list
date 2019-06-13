169a6e27a83f7b3917f02d24b3f07214e6e6ba93
Gingano <lesha.kowalenko@gmail.com>
2019-06-13 14:25:58 +0300

    adding func

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import AddingItem from './AddingItem'
 
 function App() {
   return (
     <div className="App">
-      Hello
+        <AddingItem />
     </div>
   );
 }
